09c5b261fd79f98db7ddb21f6e14c988b6384f55
Lazy-Bit-696 <lazyzbit696@gmail.com>
2022-05-24 18:08:44 +0530

    settings created

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 import tkinter # imports tkinter module which is used for GUI
+import settings # imports settings for windows size
 
 root = tkinter.Tk() # creates an instance of class Tk to form GUI window
 
 # all the codes should be between root = tkinter.Tk() and root.mainloop()
 
 root.configure(bg="black") # windows background color
-root.geometry('800x400') # windows width and height
+root.geometry(f'{settings.WIDTH}x{settings.HEIGHT}') # windows width and height
 root.title("Minesweeper Game") # windows title
 root.resizable(False, False) # to make its width and height not resizable
 
